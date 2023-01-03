98cfa63cbe8709a155706e81b064ebf529c04fad
Arijit Ghosh <arijitghosh120403@gmail.com>
2023-01-04 02:00:34 +0530

    Udate in Urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,8 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Backend API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.backend.com/policies/terms/",
+      contact=openapi.Contact(email="contact@backend.local"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('accounts.urls')),
+    #path('social_auth/', include(('social_auth.urls', 'social_auth'), namespace="social_auth")),
+    path('documentation/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
